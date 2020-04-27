9c056b010ab143344eeac45c13c483f6365f776c
Filip Jerga <jerga99@gmail.comm>
2020-04-27 18:00:20 +0200

    12 Cleanup + Debugg

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 
+import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/main.scss";
 
 
-const App = ({Component, pageProps}) => {
-  return <Component {...pageProps} />
+class App extends React.Component {
+
+  static async getInitialProps({Component, ctx}) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    debugger
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />
+  }
 }
 
 export default App;
