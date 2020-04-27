fa01671c8f463e2e1bad368b3c5db78d2217ceb1
Filip Jerga <jerga99@gmail.comm>
2020-04-27 18:17:40 +0200

    13 Adds Layout

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -2,12 +2,16 @@
 import Header from '../shared/Header';
 
 const BaseLayout = props => {
-
+  const { className, children } = props;
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      {props.children}
-    </>
+      <main className={`cover ${className}`}>
+        <div className="wrapper">
+          {children}
+        </div>
+      </main>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,52 @@
 
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-import { Button, Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 class Index extends React.Component {
   render() {
     return (
-      <BaseLayout>
-        <Container>
-          <Button color="danger">Danger!</Button>
-        </Container>
+      <BaseLayout className="cover">
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index.png" />
+          </div>
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper`}>
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png"/>
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to the portfolio website of Filip Jerga.
+                    Get informed, collaborate and discover projects I was working on through the years!
+                  </h1>
+                </div>
+                <div className="hero-welcome-bio">
+                  <h1>
+                    Let's take a look on my work.
+                  </h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </BaseLayout>
     )
   }
