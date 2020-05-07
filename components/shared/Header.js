677a892aa1ef636a59fdd296635a9a73da0841c2
Filip Jerga <jerga99@gmail.comm>
2020-05-07 12:25:59 +0200

    16 Header part2

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -7,85 +7,58 @@ import {
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
-  NavLink,
-  NavbarText
+  NavItem
 } from 'reactstrap';
 
 
-// // Todo: Transform To Class
-// const Example = (props) => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggle = () => setIsOpen(!isOpen);
-
-//   return (
-//     <div>
-//       <Navbar color="light" light expand="md">
-//         <NavbarBrand href="/">Filip Jerga</NavbarBrand>
-//         <NavbarToggler onClick={toggle} />
-//         <Collapse isOpen={isOpen} navbar>
-//           <Nav className="mr-auto" navbar>
-//             <NavItem>
-//               <Link href="/">
-//                 <a className="nav-link"> Home </a>
-//               </Link>
-//             </NavItem>
-//             <NavItem>
-//               <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-//             </NavItem>
-//           </Nav>
-//           <NavbarText>Simple Text</NavbarText>
-//         </Collapse>
-//       </Navbar>
-//     </div>
-//   );
-// }
-
-// export default Example;
-
 const BsNavLink = props => {
-  const { route, title } = props;
+  const { href, title } = props;
   return (
-    <Link href={route}>
-      <a className="nav-link">{title}</a>
+    <Link href={href}>
+      <a className="nav-link port-navbar-link">{title}</a>
     </Link>
   )
 }
 
-export default class Header extends React.Component {
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-  state = { isOpen: false }
-  toggle = () => this.setState({isOpen: !this.state.isOpen})
-
-  render() {
-    const { isOpen } = this.state;
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Filip Jerga</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <BsNavLink route="/" title="Home"/>
-              </NavItem>
-              <NavItem>
-                <BsNavLink route="/about" title="About"/>
-              </NavItem>
-              <NavItem>
-                <BsNavLink route="/portfolios" title="Portfolios"/>
-              </NavItem>
-              <NavItem>
-                <BsNavLink route="/blogs" title="Blogs"/>
-              </NavItem>
-              <NavItem>
-                <BsNavLink route="/cv" title="Cv"/>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navbar
+        className="port-navbar port-default absolute"
+        color="transparent"
+        dark
+        expand="md">
+        <NavbarBrand>
+          <Link href="/">
+            <a className="port-navbar-brand">Filip Jerga</a>
+          </Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/" title="Home"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/about" title="About"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/portfolios" title="Portfolios"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/blogs" title="Blogs"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/cv" title="Cv"/>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Header;
