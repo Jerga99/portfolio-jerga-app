3365a73c7a0385a89a44eaaf70319ee311b91df1
Filip Jerga <jerga99@gmail.comm>
2020-05-08 11:20:01 +0200

    22 Api functions 2

--- pages/api/v1/posts/index.js
+++ pages/api/v1/posts/index.js
@@ -1,9 +1,15 @@
 
+import axios from 'axios';
 
-import { data } from '@/data';
-
-function handlePosts (req, res) {
-  res.status(200).json(data);
+export default async (req, res) => {
+  try {
+    const axiosRes = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const posts = axiosRes.data;
+    res.status(200).json(posts.slice(0, 10));
+  } catch (e) {
+    console.error(e);
+    res.status(error.status || 400).end('Api Error!');
+  }
 }
 
-export default handlePosts;
+
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,16 +1,17 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import Link from 'next/link';
-import axios from 'axios';
 
-const Portfolios = ({posts}) => {
+const Portfolios = () => {
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     async function getPosts() {
       const res = await fetch('/api/v1/posts');
       const data = await res.json();
+      setPosts(data);
     }
 
     getPosts();
@@ -38,18 +39,6 @@ const Portfolios = ({posts}) => {
       </BasePage>
     </BaseLayout>
   )
-}
-
-Portfolios.getInitialProps = async () => {
-  let posts = [];
-  try {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    posts = res.data;
-  } catch(e) {
-    console.error(e);
-  }
-
-  return { posts: posts.slice(0, 10) };
 }
 
 export default Portfolios;
