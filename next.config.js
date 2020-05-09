2f6043a1c95fe6a1f06987603d20ef8c614d1193
Filip Jerga <jerga99@gmail.comm>
2020-05-09 13:54:42 +0200

    31 Auth0 environment variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   webpack: config => {
     config.resolve.alias['@'] = path.resolve(__dirname);
+    config.plugins.push(new Dotenv({silent: true}));
     return config;
   }
 }
--- utils/auth0.js
+++ utils/auth0.js
@@ -3,32 +3,13 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
 export default initAuth0({
-  domain: '<AUTH0_DOMAIN>',
-  clientId: '<AUTH0_CLIENT_ID>',
-  clientSecret: '<AUTH0_CLIENT_SECRET>',
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: 'openid profile',
-  redirectUri: 'http://localhost:3000/api/callback',
-  postLogoutRedirectUri: 'http://localhost:3000/',
+  redirectUri: process.env.AUTH0_REDIRECT_URI,
+  postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
   session: {
-    // The secret used to encrypt the cookie.
-    cookieSecret: '<RANDOMLY_GENERATED_SECRET>',
-    // The cookie lifetime (expiration) in seconds. Set to 8 hours by default.
-    cookieLifetime: 60 * 60 * 8,
-    // (Optional) The cookie domain this should run on. Leave it blank to restrict it to your domain.
-    cookieDomain: 'your-domain.com',
-    // (Optional) SameSite configuration for the session cookie. Defaults to 'lax', but can be changed to 'strict' or 'none'. Set it to false if you want to disable the SameSite setting.
-    cookieSameSite: 'lax',
-    // (Optional) Store the id_token in the session. Defaults to false.
-    storeIdToken: false,
-    // (Optional) Store the access_token in the session. Defaults to false.
-    storeAccessToken: false,
-    // (Optional) Store the refresh_token in the session. Defaults to false.
-    storeRefreshToken: false
-  },
-  oidcClient: {
-    // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
-    httpTimeout: 2500,
-    // (Optional) Configure the clock tolerance in milliseconds, if the time on your server is running behind.
-    clockTolerance: 10000
+    cookieSecret: process.env.AUTH0_COOKIE_SECRET,
   }
 });
