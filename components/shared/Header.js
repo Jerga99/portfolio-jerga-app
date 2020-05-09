dec6b1cf4d964f273e2dbd8fafb87ec9e942eda2
Filip Jerga <jerga99@gmail.comm>
2020-05-09 13:26:03 +0200

    29-30 Auth0 Registration + Api Login route

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -25,7 +25,7 @@ const BsNavBrand = () =>
   </Link>
 
 const LoginLink = () =>
-  <span className="nav-link port-navbar-link clickable">Login</span>
+  <BsNavLink href="/api/v1/login" title="Login" />
 
 const LogoutLink = () =>
   <span className="nav-link port-navbar-link clickable">Logout</span>
---
+++ pages/api/v1/login.js
@@ -0,0 +1,6 @@
+
+
+
+export default function login(req, res) {
+  res.status(200).json({message: 'Hello from login route!'})
+}
