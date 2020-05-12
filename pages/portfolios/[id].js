30bc5dd16cc4e051d94359fab1fcf4969903d9bc
Filip Jerga <jerga99@gmail.comm>
2020-05-12 17:46:18 +0200

    52 Get static props

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -18,11 +18,33 @@ const Portfolio = ({portfolio}) => {
   )
 }
 
-export async function getServerSideProps({query}) {
-  const json = await new PortfolioApi().getById(query.id);
-  const portfolio = json.data;
+// export async function getServerSideProps({query}) {
+//   const json = await new PortfolioApi().getById(query.id);
+//   const portfolio = json.data;
 
-  return {props: { portfolio }};
+//   return {props: { portfolio }};
+// }
+
+// This function is executed at the build time
+export async function getStaticPaths() {
+  const json = await new PortfolioApi().getAll();
+  const portfolios = json.data;
+
+  // Get the paths we want pre-render based on portfolio ID
+  const paths = portfolios.map(portfolio => {
+    return {
+      params: {id: portfolio._id}
+    }
+  })
+
+  // fallback: false means that "not found pages" will be resolved into 404 page
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({params}) {
+  const json = await new PortfolioApi().getById(params.id);
+  const portfolio = json.data;
+  return { props: {portfolio}};
 }
 
 export default Portfolio;
