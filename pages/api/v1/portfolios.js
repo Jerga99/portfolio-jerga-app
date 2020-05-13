6ed9f48600f5c1144233d68041989f7d4f1f442c
Filip Jerga <jerga99@gmail.comm>
2020-05-13 16:09:58 +0200

    59 Get access token

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -1,11 +1,13 @@
 
 
 import PortfolioApi from '@/lib/api/portfolios';
+import auth0 from '@/utils/auth0';
 
 export default async function createPortfolio(req, res) {
   try {
-    const data = req.body;
-    await new PortfolioApi().createPortfolio(data);
+    const { accessToken } = await auth0.getSession(req);
+    console.log(accessToken)
+    await new PortfolioApi().createPortfolio(req.body);
     return res.json({message: 'Portfolio was created!'});
   } catch(e) {
     return res.status(e.status || 400).end(e.message);
--- utils/auth0.js
+++ utils/auth0.js
@@ -8,10 +8,12 @@ const auth0 = initAuth0({
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: 'openid profile',
+  audience: process.env.AUTH0_AUDIENCE,
   redirectUri: process.env.AUTH0_REDIRECT_URI,
   postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
   session: {
     cookieSecret: process.env.AUTH0_COOKIE_SECRET,
+    storeAccessToken: true
   }
 });
 
