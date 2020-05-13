d4748610a68f96a571f41d892c77c19107847e3c
Filip Jerga <jerga99@gmail.comm>
2020-05-13 16:55:35 +0200

    61 use create portfolio

--- actions/portfolios.js
+++ actions/portfolios.js
@@ -1,6 +1,31 @@
 
 import axios from 'axios';
+import { useState } from 'react';
 
-export function createPortfolio(data) {
+function createPortfolio(data) {
   return axios.post('/api/v1/portfolios', data);
+}
+
+
+export function useCreatePortfolio() {
+  const [reqState, setReqState] = useState({
+    error: null,
+    data: null,
+    loading: false
+  });
+
+  const createPortfolioHandler = async (...data) => {
+    setReqState({error: null, data: null, loading: true});
+    try {
+      const json = await createPortfolio(...data);
+      debugger
+      setReqState({error: null, data: json.data, loading: false});
+    } catch(e) {
+      debugger
+      const message = (e.response && e.response.message) || 'Ooops, something went wrong...';
+      setReqState({error: message, data: null, loading: false});
+    }
+  }
+
+  return [createPortfolioHandler, {...reqState}]
 }
--- pages/portfolios/new.js
+++ pages/portfolios/new.js
@@ -7,9 +7,11 @@ import BasePage from '@/components/BasePage';
 import withAuth from '@/hoc/withAuth';
 import { Row, Col } from 'reactstrap';
 import PortfolioForm from '@/components/PortfolioForm';
-import { createPortfolio } from '@/actions/portfolios';
+import { useCreatePortfolio } from '@/actions/portfolios';
 
 const PortfolioNew = ({user, loading: userLoading}) => {
+  const [createPortfolio, {data, loading, error}] = useCreatePortfolio();
+  debugger
 
   const _createPortfolio = (data) => {
     createPortfolio(data)
