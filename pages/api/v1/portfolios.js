40efd262c6f18328940d03d1865758280ad13951
Filip Jerga <jerga99@gmail.comm>
2020-05-13 16:22:37 +0200

    60 Create portfolio from App

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -6,9 +6,8 @@ import auth0 from '@/utils/auth0';
 export default async function createPortfolio(req, res) {
   try {
     const { accessToken } = await auth0.getSession(req);
-    console.log(accessToken)
-    await new PortfolioApi().createPortfolio(req.body);
-    return res.json({message: 'Portfolio was created!'});
+    const json = await new PortfolioApi(accessToken).createPortfolio(req.body);
+    return res.json(json.data);
   } catch(e) {
     return res.status(e.status || 400).end(e.message);
   }
