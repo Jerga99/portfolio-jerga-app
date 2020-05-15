4089fd35a158ea4cc541d4b3f842956904f080b0
Filip Jerga <jerga99@gmail.comm>
2020-05-15 13:39:53 +0200

    81 Base api

--- pages/api/v1/portfolios/index.js
+++ pages/api/v1/portfolios/index.js
@@ -6,7 +6,7 @@ import auth0 from '@/utils/auth0';
 export default async function createPortfolio(req, res) {
   try {
     const { accessToken } = await auth0.getSession(req);
-    const json = await new PortfolioApi(accessToken).createPortfolio(req.body);
+    const json = await new PortfolioApi(accessToken).create(req.body);
     return res.json(json.data);
   } catch(e) {
     return res.status(e.status || 422).json(e.response.data);
