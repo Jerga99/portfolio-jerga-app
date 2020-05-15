ebbb458450b81c297d934d25dcb3dfb5edb74200
Filip Jerga <jerga99@gmail.comm>
2020-05-15 17:19:43 +0200

    84 Filter Blogs

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,12 +4,26 @@ import BasePage from '@/components/BasePage';
 import { withAuth } from 'utils/auth0';
 import { Row, Col } from 'reactstrap';
 import Masthead from 'components/shared/Masthead';
+import Link from 'next/link';
 
 import auth0 from 'utils/auth0';
 import BlogApi from 'lib/api/blogs';
 
 const Dashboard = ({user, blogs}) => {
-  debugger
+
+  const renderBlogs = (blogs, status) => (
+    <ul className="user-blogs-list">
+      { blogs.filter(blog => blog.status === status).map(blog =>
+        <li key={blog._id}>
+          <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
+            <a>{blog.title}</a>
+          </Link>
+        </li>
+        )
+      }
+    </ul>
+  )
+
   return (
     <BaseLayout navClass="transparent" user={user} loading={false}>
       <Masthead imagePath="/images/home-bg.jpg" />
@@ -17,9 +31,11 @@ const Dashboard = ({user, blogs}) => {
         <Row>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Published Blogs </h2>
+            {renderBlogs(blogs, 'published')}
           </Col>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Draft Blogs </h2>
+            {renderBlogs(blogs, 'draft')}
           </Col>
         </Row>
       </BasePage>
