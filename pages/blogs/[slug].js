c93bce8f537975f313757f26d525c7d6d30d46b3
Filip Jerga <jerga99@gmail.comm>
2020-05-16 14:12:35 +0200

    92 Blog Detail Page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,17 +1,40 @@
 
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
+import { Row, Col } from 'reactstrap';
 import { useGetUser } from '@/actions/user';
+import { SlateView } from 'slate-simple-editor';
 
-const BlogDetail = () => {
+import BlogApi from 'lib/api/blogs';
+
+const BlogDetail = ({blog}) => {
+  debugger
   const { data, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
-      <BasePage>
-        <h1>I am Blog Detail</h1>
+      <BasePage className="slate-container">
+        <Row>
+          <Col md={{size: 8, offset: 2}}>
+            <SlateView initialContent={blog.content}/>
+          </Col>
+        </Row>
       </BasePage>
     </BaseLayout>
   )
 }
+
+export async function getStaticPaths() {
+  const json = await new BlogApi().getAll();
+  const blogs = json.data;
+  const paths = blogs.map(b => ({params: { slug: b.slug}}));
+  return { paths, fallback: false};
+}
+
+export async function getStaticProps({params}) {
+  const json = await new BlogApi().getBySlug(params.slug);
+  return {props: {blog: json.data}}
+}
+
+
 
 export default BlogDetail;
