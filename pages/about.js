1d184c6a8283e175b4ba95c5564955c1c0ccdc04
Filip Jerga <jerga99@gmail.comm>
2020-05-17 11:53:49 +0200

    94 About Page

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,57 @@
 
+
+import { useEffect } from 'react';
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import { useGetUser } from '@/actions/user';
+import { Row, Col } from 'reactstrap';
 
 const About = () => {
   const { data, loading } = useGetUser();
+
+  useEffect(() => {
+    return () => {
+      window.__isAboutLoaded = true;
+    }
+  })
+
+  const createFadeInClass = () => {
+    if (typeof window !== 'undefined') {
+      return window.__isAboutLoaded ? '' : 'fadein';
+    }
+
+    return 'fadein';
+  }
+
+
   return (
     <BaseLayout user={data} loading={loading}>
-      <BasePage>
-        <h1>I am About Page</h1>
+      <BasePage className="about-page">
+        <Row className="mt-5">
+          <Col md="6">
+            <div className="left-side">
+              <h1 className={`title ${createFadeInClass()}`}>Hello, Welcome</h1>
+              <h4 className={`subtitle ${createFadeInClass()}`}>To About Page</h4>
+              <p className={`subsubTitle ${createFadeInClass()}`}>Feel free to read short description about me.</p>
+            </div>
+          </Col>
+          <Col md="6">
+            <div className={`${createFadeInClass()}`}>
+              <p>My name is Filip Jerga and I am an experienced software engineer and freelance developer. </p>
+              <p>
+              I have a Master's degree in Artificial Intelligence and several years of experience working
+              on a wide range of technologies and projects from C++ development for ultrasound devices to
+                modern mobile and web applications in React and Angular.
+              </p>
+              <p>
+              Throughout my career, I have acquired advanced technical knowledge and the ability to explain
+              programming topics clearly and in detail to a broad audience. I invite you to take my course,
+              where I have put a lot of effort to explain web and software engineering concepts in a detailed,
+              hands-on and understandable way.
+              </p>
+            </div>
+          </Col>
+        </Row>
       </BasePage>
     </BaseLayout>
   )
