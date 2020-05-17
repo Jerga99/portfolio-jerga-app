73ae3e85dcf5a42811fe744d370868eb88a9171e
Filip Jerga <jerga99@gmail.comm>
2020-05-17 13:53:53 +0200

    98 CV Page

--- pages/cv.js
+++ pages/cv.js
@@ -3,13 +3,18 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import { useGetUser } from '@/actions/user';
+import { Row, Col } from 'reactstrap';
 
 const Cv = () => {
   const { data, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
       <BasePage>
-        <h1>I am Cv Page</h1>
+        <Row>
+          <Col md={{size: 8, offset: 2}}>
+            <iframe style={{width: '100%', height: '800px'}} src="/jerga_cv.pdf"/>
+          </Col>
+        </Row>
       </BasePage>
     </BaseLayout>
   )
