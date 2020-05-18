09e5fc237b10641bd7a606d243cc1735a5363829
Filip Jerga <jerga99@gmail.comm>
2020-05-18 12:53:34 +0200

    104 Open Graph

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,8 +1,10 @@
 
 import { Container } from 'reactstrap';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const BasePage = props => {
+  const router = useRouter();
   const {
     indexPage,
     className = '',
@@ -17,14 +19,14 @@ const BasePage = props => {
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          key="description"
-          content={metaDescription} />
-        <meta
-          name="title"
-          key="title"
-          content={title} />
+        <meta name="description" key="description" content={metaDescription} />
+        <meta name="title" key="title" content={title} />
+        <meta property="og:title" key="og:title" content={title} />
+        <meta property="og:locale" key="og:locale" content="en_EU" />
+        <meta property="og:url" key="og:url" content={`${process.env.BASE_URL}${router.asPath}`} />
+        <meta property="og:type" key="og:type" content="website" />
+        <meta property="og:description" key="og:description" content={metaDescription} />
+        <meta property="og:image" key="og:image" content={`${process.env.BASE_URL}/images/section-1.png`} />
       </Head>
       <div className={`${pageType} ${className}`}>
         <Container>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   env: {
-    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
+    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+    BASE_URL: process.env.BASE_URL
   }
 }
