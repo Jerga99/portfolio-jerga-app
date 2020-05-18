51d3071d8a6e8063d114314c2d360d60aaf5397f
Filip Jerga <jerga99@gmail.comm>
2020-05-18 12:07:40 +0200

    102 Index Page type

--- components/BasePage.js
+++ components/BasePage.js
@@ -4,17 +4,20 @@ import Head from 'next/head';
 
 const BasePage = props => {
   const {
+    indexPage,
     className = '',
     header,
     title = "Portfolio - Filip Jerga",
     children } = props;
+
+  const pageType = indexPage ? 'index-page' : 'base-page';
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>{title}</title>
       </Head>
-      <div className={`base-page ${className}`}>
+      <div className={`${pageType} ${className}`}>
         <Container>
           { header &&
             <div className="page-header">
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 
 import { useState, useRef, useEffect } from 'react';
 import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
 import { Container, Row, Col } from 'reactstrap';
 import Typed from 'react-typed';
 import { useGetUser } from '@/actions/user';
@@ -28,69 +29,71 @@ const Index = () => {
       loading={loading}
       navClass="transparent"
       className={`cover ${isFlipping ? 'cover-orange' : 'cover-blue'}`}>
-      <div className="main-section">
-        <div className="background-image">
-          <img src="/images/background-index.png" />
-        </div>
-        <Container>
-          <Row>
-            <Col md="6">
-              <div className="hero-section">
-                <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
-                  <div className="front">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio and job history.
+      <BasePage indexPage title="Portfolio - Filip Jerga">
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index.png" />
+          </div>
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
+                    <div className="front">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png"/>
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
                       </div>
                     </div>
-                    <img className="image" src="/images/section-1.png"/>
-                    <div className="shadow-custom">
-                      <div className="shadow-inner"> </div>
-                    </div>
-                  </div>
-                  <div className="back">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio and job history.
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
                       </div>
-                    </div>
-                    <img className="image" src="/images/section-2.png"/>
-                    <div className="shadow-custom shadow-custom-orange">
-                      <div className="shadow-inner"> </div>
+                      <img className="image" src="/images/section-2.png"/>
+                      <div className="shadow-custom shadow-custom-orange">
+                        <div className="shadow-inner"> </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Col>
-            <Col md="6" className="hero-welcome-wrapper">
-              <div className="hero-welcome-text">
-                <h1>
-                  Welcome to the portfolio website of Filip Jerga.
-                  Get informed, collaborate and discover projects I was working on through the years!
-                </h1>
-              </div>
-              <Typed
-                loop
-                typeSpeed={70}
-                backSpeed={70}
-                strings={ROLES}
-                backDelay={1000}
-                loopCount={0}
-                showCursor
-                className="self-typed"
-                cursorChar="|"
-              />
-              <div className="hero-welcome-bio">
-                <h1>
-                  Let's take a look on my work.
-                </h1>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to the portfolio website of Filip Jerga.
+                    Get informed, collaborate and discover projects I was working on through the years!
+                  </h1>
+                </div>
+                <Typed
+                  loop
+                  typeSpeed={70}
+                  backSpeed={70}
+                  strings={ROLES}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                />
+                <div className="hero-welcome-bio">
+                  <h1>
+                    Let's take a look on my work.
+                  </h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </BasePage>
     </BaseLayout>
   )
 }
